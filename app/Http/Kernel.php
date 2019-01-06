4268bc46301d12bc1a5d9cb37c5188d1696aa9cd
Memo Chou <memochou1993@gmail.com>
2019-01-06 21:16:26 +0800

    將中介層 AddHeaders 改名為 SetHeaders

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'headers' => \App\Http\Middleware\AddHeaders::class,
+        'headers' => \App\Http\Middleware\SetHeaders::class,
     ];
 
     /**
--- app/Http/Middleware/SetHeaders.php
+++ app/Http/Middleware/SetHeaders.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class AddHeaders
+class SetHeaders
 {
     public function handle($request, Closure $next)
     {
